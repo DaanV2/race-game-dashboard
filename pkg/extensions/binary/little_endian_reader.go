f4e5c3727d74516bd32dc9c12f85a62a8cab16db
DaanV2 <daanverstraten@hotmail.com>
2026-08-22 23:58:18 +0200

    adding little endian reader

---
+++ pkg/extensions/binary/little_endian_reader.go
@@ -0,0 +1,75 @@
+package xbinary
+
+import (
+	"encoding/binary"
+	"unsafe"
+)
+
+type LittleEndianReader struct {
+	index int
+	buf   []byte
+}
+
+func NewLittleEndianReader(buf []byte) LittleEndianReader {
+	return LittleEndianReader{
+		index: 0,
+		buf:   buf,
+	}
+}
+
+func (r *LittleEndianReader) ReadUInt8() uint8 {
+	v := r.buf[r.index]
+	r.index++
+
+	return v
+}
+
+func (r *LittleEndianReader) ReadByte() byte {
+	v := r.buf[r.index]
+	r.index++
+
+	return v
+}
+
+func (r *LittleEndianReader) ReadInt8() int8 {
+	v := r.buf[r.index]
+	r.index++
+
+	return int8(v)
+}
+func (r *LittleEndianReader) ReadUInt16() uint16 {
+	v := binary.LittleEndian.Uint16(r.buf[r.index:])
+	r.index += int(unsafe.Sizeof(v))
+
+	return v
+}
+func (r *LittleEndianReader) ReadInt16() int16 {
+	v := int16(binary.LittleEndian.Uint16(r.buf[r.index:]))
+	r.index += int(unsafe.Sizeof(v))
+
+	return v
+}
+func (r *LittleEndianReader) ReadUInt32() uint32 {
+	v := binary.LittleEndian.Uint32(r.buf[r.index:])
+	r.index += int(unsafe.Sizeof(v))
+
+	return v
+}
+func (r *LittleEndianReader) ReadFloat32() float32 {
+	v := float32(binary.LittleEndian.Uint32(r.buf[r.index:]))
+	r.index += int(unsafe.Sizeof(v))
+
+	return v
+}
+func (r *LittleEndianReader) ReadFloat64() float64 {
+	v := float64(binary.LittleEndian.Uint64(r.buf[r.index:]))
+	r.index += int(unsafe.Sizeof(v))
+
+	return v
+}
+func (r *LittleEndianReader) ReadUInt64() uint64 {
+	v := binary.LittleEndian.Uint64(r.buf[r.index:])
+	r.index += int(unsafe.Sizeof(v))
+
+	return v
+}
